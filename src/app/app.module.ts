9bdd6af4d2aeb2c52cb33860702486fb22057501
Chunkit Yip <98427054+ckyrevature@users.noreply.github.com>
2022-06-22 16:44:41 -0400

    search bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//FormsModule is used to make all the necessary imports for form implementation
+import { FormsModule } from '@angular/forms';
+/*
+Communicate with a server over the HTTP protocol ,to download or upload 
+data and access other back-end services
+*/
+import { HttpClientModule } from '@angular/common/http';
+
+//The following is Material module
+//To build design components for desktop and mobile web applications
+import { MatTabsModule } from '@angular/material/tabs'; 
+import { MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
